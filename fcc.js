4a438fe345405a9b4baac6418bbb3781ebcac8e1
heckdavid <99364904+heckdavid@users.noreply.github.com>
2024-06-23 19:27:05 -0400

    Update fcc.js

--- fcc.js
+++ fcc.js
@@ -1,3 +1,9 @@
-// Learning introductory javascript by building a pyramid generator Step 1 //
+// Learning introductory javascript by building a pyramid generator //
+
+// Step 1 //
 let character;
+
+// Step 2 //
+
+let character = "Hello";
 
