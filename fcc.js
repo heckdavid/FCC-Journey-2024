cc5b566f49a93efa5149aa996b89db681f62cbb1
heckdavid <99364904+heckdavid@users.noreply.github.com>
2024-06-27 09:37:42 -0400

    Update fcc.js

--- fcc.js
+++ fcc.js
@@ -813,10 +813,72 @@ console.log(result);
 
 // Step 69 //
 
+const character = "#";
+const count = 8;
+const rows = [];
 
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
 
+for (let i = 0; i < count; i = i + 1) {
+  rows.push(padRow(i + 1, count));
+}
 
+let result = ""
 
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
+
+// Step 70 //
+
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
+console.log(padRow);
+
+for (let i = 0; i < count; i = i + 1) {
+  rows.push(padRow(i + 1, count));
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
+
+// Step 71 //
+
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
+
+for (let i = 0; i < count; i += 1) {
+  rows.push(padRow(i + 1, count));
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
+
+// Step 72 // 
 
 
 
