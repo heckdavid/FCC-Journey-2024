5115dd18b9d26d4016deede4b0b78d7ecc401f4f
heckdavid <99364904+heckdavid@users.noreply.github.com>
2024-07-04 19:53:18 -0400

    Update fcc.js

--- fcc.js
+++ fcc.js
@@ -2398,10 +2398,80 @@ console.log(hasPassingGrade(100));
 console.log(hasPassingGrade(53));
 console.log(hasPassingGrade(87));
 
-// Step 4 //
+// Step 4 FINAL STEP //
+
+function getAverage(scores) {
+  let sum = 0;
+
+  for (const score of scores) {
+    sum += score;
+  }
+
+  return sum / scores.length;
+}
+
+function getGrade(score) {
+  if (score === 100) {
+    return "A++";
+  } else if (score >= 90) {
+    return "A";
+  } else if (score >= 80) {
+    return "B";
+  } else if (score >= 70) {
+    return "C";
+  } else if (score >= 60) {
+    return "D";
+  } else {
+    return "F";
+  }
+}
+
+function hasPassingGrade(score) {
+  return getGrade(score) !== "F";
+}
+
+/* Complete the studentMsg function with totalScores and studentScore for parameters. The function should return a string representing a message to the student.
+
+If the student passed the course, the string should follow this format:
+
+Class average: average-goes-here. Your grade: grade-goes-here. You passed the course.
 
 
+Example Code
+Class average: average-goes-here. Your grade: grade-goes-here. You passed the course.
+If the student failed the course, the string should follow this format:
 
+Example Code
+
+Class average: average-goes-here. Your grade: grade-goes-here. You failed the course.
+
+
+Use the getAverage function to get the class average.
+Use the getGrade function to get the student's grade.
+Use string concatenation (+) to build the message.
+Be careful with the punctuation and spaces in the message.
+*/ 
+
+/* COMMENTS TO TRY STUFF:
+
+Console message should say...
+Your function call of studentMsg([92, 88, 12, 77, 57, 100, 67, 38, 97, 89], 37) should return the following message: "Class average: 71.7. Your grade: F. You failed the course.".
+Your function call of studentMsg([56, 23, 89, 42, 75, 11, 68, 34, 91, 19], 100) should return the following message: "Class average: 50.8. Your grade: A++. You passed the course.".
+Your studentMsg function should return the correct message based on the student's score and the class average.
+*/
+
+function studentMsg(totalScores, studentScore) {
+  if (hasPassingGrade(studentScore)) {
+      return "Class average: " + getAverage(totalScores) + "." + " Your grade: " + getGrade(studentScore) + "." + " You passed the course.";
+  }   else {   
+      return "Class average: " + getAverage(totalScores) + "." + " Your grade: " + getGrade(studentScore) + "." + " You failed the course.";
+
+  }  
+}
+console.log(studentMsg([92, 88, 12, 77, 57, 100, 67, 38, 97, 89], 37));
+console.log(studentMsg([56, 23, 89, 42, 75, 11, 68, 34, 91, 19], 100));
+
+// 
 
 
 
